d248c3b5605ee2a04108fb41cd6793f47a0d68c6
Thomas <Thomas@100.83.9.139>
2017-04-19 16:51:27 +0200

    T

--- ViperSerialization/src/Serialization/TestClass.java
+++ ViperSerialization/src/Serialization/TestClass.java
@@ -8,7 +8,7 @@ public class TestClass
 	
 	public static void main(String[] args)
 	{
-		int[] data = new int[50000];
+		int[] data = new int[50];
 		for (int i = 0; i < data.length; i++)
 		{
 			data[i] = random.nextInt();
